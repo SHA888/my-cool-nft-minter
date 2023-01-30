f5ce7c63fa8892c5bee75bb54d0739a1e899f173
Kresna Sucandra <kresnasucandra@gmail.com>
2023-01-30 10:57:21 +0800

    feat: update minter with interact and pinata

--- src/Minter.js
+++ src/Minter.js
@@ -1,4 +1,9 @@
 import { useEffect, useState } from "react";
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  mintNFT,
+} from "./utils/interact.js";
 
 const Minter = (props) => {
 
@@ -8,17 +13,49 @@ const Minter = (props) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
  
-  useEffect(async () => { //TODO: implement
-    
+  useEffect(async () => {
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status);
+
+    addWalletListener();
   }, []);
 
-  const connectWalletPressed = async () => { //TODO: implement
-   
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
   };
 
-  const onMintPressed = async () => { //TODO: implement
-    
+  const onMintPressed = async () => {
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
   };
 
   return (
@@ -35,7 +72,8 @@ const Minter = (props) => {
       </button>
 
       <br></br>
-      <h1 id="title">🧙‍♂️ Alchemy NFT Minter</h1>
+      <h1 id="title">🧙‍♂️ MyCoolNFT Minter</h1>
+      <p>Decentralized Application (dApp) to MINT your NFT</p>
       <p>
         Simply add your asset's link, name, and description, then press "Mint."
       </p>
